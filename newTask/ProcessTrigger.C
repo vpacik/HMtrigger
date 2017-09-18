841e72c23c3077fe5f53d5b247fe71819b186413
vpacik <vojtech.pacik@cern.ch>
2017-09-18 12:16:02 +0200

    Drawing (and saving) canvases moved to PlotTrigger macro

--- newTask/ProcessTrigger.C
+++ newTask/ProcessTrigger.C
@@ -19,10 +19,10 @@ void ProcessTrigger(Int_t iNumEventsToProcess = -1)
 {
   // parameters
   // const char* sInputFile = "/Users/vpacik/NBI/triggerHMstudies/newTask/running/16k/AnalysisResults_BK.root";
-  const char* sInputFile = "/Users/vpacik/NBI/triggerHMstudies/newTask/running/15l/AnalysisResults.root";
+  const char* sInputFile = "/Users/vpacik/NBI/triggerHMstudies/newTask/running/16o/AnalysisResults.root";
   // const char* sInputFile = "/Users/vpacik/NBI/triggerHMstudies/newTask/AnalysisResults.root";
 
-  TString sOutputPath = "/Users/vpacik/NBI/triggerHMstudies/newTask/running/15l/";
+  TString sOutputPath = "/Users/vpacik/NBI/triggerHMstudies/newTask/running/16o/";
   // TString sOutputPath = "/Users/vpacik/NBI/triggerHMstudies/newTask/";
 
   // Int_t iNumEventsToProcess = -1; // number of events to be processed; if -1 all are processed
@@ -161,7 +161,7 @@ void ProcessTrigger(Int_t iNumEventsToProcess = -1)
   TH2D* h2FOROnOffINT7 = new TH2D("h2FOROnOffINT7","FastOr offline x online (INT7); FOR offline; FOR online", 800,0,800, 800,0,800);
   TH2D* h2FOROnOffINT7PhysSel = new TH2D("h2FOROnOffINT7PhysSel","FastOr offline x online (INT7 && PhysSel); FOR offline; FOR online", 800,0,800, 800,0,800);
   TH2D* h2FOROnOffVHMSH2 = new TH2D("h2FOROnOffVHMSH2","FastOr offline x online (VHMSH2); FOR offline; FOR online", 800,0,800, 800,0,800);
-  TH2D* h2FOROnOffVHMSH2PhysSel = new TH2D("h2FOROnOffVHMSH2Phys","FastOr offline x online (VHMSH2 && PhysSel); FOR offline; FOR online", 800,0,800, 800,0,800);
+  TH2D* h2FOROnOffVHMSH2PhysSel = new TH2D("h2FOROnOffVHMSH2PhysSel","FastOr offline x online (VHMSH2 && PhysSel); FOR offline; FOR online", 800,0,800, 800,0,800);
   TH2D* h2FOROffTrackletsINT7PhysSel = new TH2D("h2FOROffTrackletsINT7PhysSel","tracklets x FastOR (INT7 && PhysSel); # tracklets; FOR offline", 200,0,200, 200,0,200);
   TH2D* h2FOROffTrackletsVHMSH2PhysSel = new TH2D("h2FOROffTrackletsVHMSH2PhysSel","tracklets x FastOR (VHMSH2 && PhysSel); # tracklets; FOR offline;", 200,0,200, 200,0,200);
   TH2D* h2TrackletsTracksINT7PhysSel = new TH2D("h2TrackletsTracksINT7PhysSel","tracklets x ESD tracks (INT7 && PhysSel); # tracklets; # ESD tracks", 200,0,200, 2000,0,2000);
@@ -384,11 +384,11 @@ void ProcessTrigger(Int_t iNumEventsToProcess = -1)
   hPtTurnOnAll->Divide(hPtDistVHMSH2PhysSelAll,hPtDistINT7PhysSelAll,1.,1.);
   hPtTurnOnAll->SetTitle("Pt turn-on (all); track pt (GeV/c); trigger/INT7");
 
-  TH1D* hPtTurnOnThrs90 = (TH1D*) hPtDistVHMSH2PhysSelThrs90->Clone("hPtTurnOnAll");
+  TH1D* hPtTurnOnThrs90 = (TH1D*) hPtDistVHMSH2PhysSelThrs90->Clone("hPtTurnOnThrs90");
   hPtTurnOnThrs90->Divide(hPtDistVHMSH2PhysSelThrs90,hPtDistINT7PhysSelThrs90,1.,1.);
   hPtTurnOnThrs90->SetTitle("Pt turn-on (90%); track pt (GeV/c); trigger/INT7");
 
-  TH1D* hPtTurnOnThrs95 = (TH1D*) hPtDistVHMSH2PhysSelThrs95->Clone("hPtTurnOnAll");
+  TH1D* hPtTurnOnThrs95 = (TH1D*) hPtDistVHMSH2PhysSelThrs95->Clone("hPtTurnOnThrs95");
   hPtTurnOnThrs95->Divide(hPtDistVHMSH2PhysSelThrs95,hPtDistINT7PhysSelThrs95,1.,1.);
   hPtTurnOnThrs95->SetTitle("Pt turn-on (95%); track pt (GeV/c); trigger/INT7");
 
@@ -441,229 +441,7 @@ void ProcessTrigger(Int_t iNumEventsToProcess = -1)
     hPtTurnOnThrs90->Write();
     hPtTurnOnThrs95->Write();
   }
-
-  // Drawing stuff
-  TLatex* latexINT7 = new TLatex();
-  latexINT7->SetNDC();
-  latexINT7->SetTextColor(kBlue);
-  TLatex* latexVHMSH2 = new TLatex();
-  latexVHMSH2->SetNDC();
-  latexVHMSH2->SetTextColor(kRed);
-
-  TCanvas* canPurity = new TCanvas("canPurity","canPurity",1000,1000);
-  canPurity->Divide(2,2);
-  canPurity->cd(1);
-  gPad->SetLogy();
-  hEventMultINT7->SetLineColor(kBlue);
-  hEventMultINT7->SetFillColor(kBlue);
-  hEventMultINT7->SetFillStyle(3004);
-  hEventMultINT7PhysSel->SetLineColor(kRed);
-  hEventMultINT7PhysSel->SetFillColor(kRed);
-  hEventMultINT7PhysSel->SetFillStyle(3005);
-  hEventMultINT7PhysSelEventCuts->SetLineColor(kGreen+2);
-  hEventMultINT7PhysSelEventCuts->SetFillColor(kGreen+2);
-  hEventMultINT7PhysSelEventCuts->SetFillStyle(3013);
-  hEventMultINT7->Draw();
-  hEventMultINT7PhysSel->Draw("same");
-  // hEventMultINT7PhysSelEventCuts->Draw("same");
-  latexINT7->DrawLatex(0.5,0.65,"Before PhysSel");
-  latexVHMSH2->DrawLatex(0.5,0.7,"After PhysSel");
-
-
-  canPurity->cd(2);
-  gPad->SetLogy();
-  hEventMultCVHMSH2->SetLineColor(kBlue);
-  hEventMultCVHMSH2->SetFillColor(kBlue);
-  hEventMultCVHMSH2->SetFillStyle(3004);
-  hEventMultCVHMSH2PhysSel->SetLineColor(kRed);
-  hEventMultCVHMSH2PhysSel->SetFillColor(kRed);
-  hEventMultCVHMSH2PhysSel->SetFillStyle(3005);
-  hEventMultCVHMSH2PhysSelEventCuts->SetLineColor(kGreen+2);
-  hEventMultCVHMSH2PhysSelEventCuts->SetFillColor(kGreen+2);
-  hEventMultCVHMSH2PhysSelEventCuts->SetFillStyle(3013);
-  hEventMultCVHMSH2->Draw();
-  hEventMultCVHMSH2PhysSel->Draw("same");
-  // hEventMultCVHMSH2PhysSelEventCuts->Draw("same");
-  latexINT7->DrawLatex(0.5,0.65,"Before PhysSel");
-  latexVHMSH2->DrawLatex(0.5,0.7,"After PhysSel");
-
-  canPurity->cd(3);
-  hPurityINT7->SetMarkerStyle(kOpenCircle);
-  hPurityINT7->SetMarkerSize(0.8);
-  hPurityINT7->SetMarkerColor(kRed);
-  hPurityINT7->SetStats(0);
-  hPurityINT7->Draw("p0");
-
-  canPurity->cd(4);
-  hPurityCVHMSH2->SetMarkerStyle(kOpenCircle);
-  hPurityCVHMSH2->SetMarkerSize(0.8);
-  hPurityCVHMSH2->SetMarkerColor(kRed);
-  hPurityCVHMSH2->SetStats(0);
-  hPurityCVHMSH2->Draw("p0");
-  canPurity->SaveAs(Form("%s/canPurity.pdf",sOutputPath.Data()),"pdf");
-
-
-  // efficiency
-  TLine* lineThrs90 = new TLine();
-  lineThrs90->SetLineColor(kRed);
-  lineThrs90->SetLineWidth(2);
-  TLine* lineThrs95 = new TLine();
-  lineThrs95->SetLineColor(kGreen+2);
-  lineThrs95->SetLineWidth(2);
-  TLatex* latexThrs90 = new TLatex();
-  latexThrs90->SetNDC();
-  latexThrs90->SetTextColor(kRed);
-  TLatex* latexThrs95 = new TLatex();
-  latexThrs95->SetNDC();
-  latexThrs95->SetTextColor(kGreen+2);
-
-  TCanvas* canEff = new TCanvas("canEff","canEff",1000,500);
-  canEff->Divide(2,1);
-  canEff->cd(1);
-  gPad->SetLogy();
-  hEventMultINT7PhysSel->SetLineColor(kBlue);
-  hEventMultINT7PhysSel->SetFillColor(kBlue);
-  hEventMultINT7PhysSel->SetFillStyle(3004);
-  hEventMultINT7PhysSel->SetLineColor(kBlue);
-  hEventMultINT7PhysSel->SetFillColor(kBlue);
-  hEventMultINT7PhysSel->SetFillStyle(3004);
-  hEventMultINT7PhysSel->Draw();
-  hEventMultCVHMSH2PhysSel->Draw("same");
-  lineThrs90->DrawLine(iThrs90,0.,iThrs90,hEventMultINT7PhysSel->GetMaximum());
-  lineThrs95->DrawLine(iThrs95,0.,iThrs95,hEventMultINT7PhysSel->GetMaximum());
-  latexThrs90->DrawLatex(0.46,0.65, TString::Format("90%% (%d): %g",iThrs90,dTurnOn90).Data());
-  latexThrs95->DrawLatex(0.46,0.7, TString::Format("95%% (%d): %g",iThrs95,dTurnOn95).Data());
-  canEff->cd(2);
-  hEffPhysSel->SetStats(0);
-  hEffPhysSel->Draw();
-  lineThrs90->DrawLine(iThrs90,0.,iThrs90,1.);
-  lineThrs95->DrawLine(iThrs95,0.,iThrs95,1.);
-  canEff->SaveAs(Form("%s/canEff.pdf",sOutputPath.Data()),"pdf");
-
-
-  // pt turn on
-  TCanvas* canPtTurn = new TCanvas("canPtTurn","canPtTurn",1500,1000);
-  canPtTurn->Divide(3,2);
-  canPtTurn->cd(1);
-  gPad->SetLogy();
-  hPtDistINT7PhysSelAll->SetLineColor(kBlue);
-  hPtDistINT7PhysSelAll->SetFillColor(kBlue);
-  hPtDistINT7PhysSelAll->SetFillStyle(3004);
-  hPtDistVHMSH2PhysSelAll->SetLineColor(kRed);
-  hPtDistVHMSH2PhysSelAll->SetFillColor(kRed);
-  hPtDistVHMSH2PhysSelAll->SetFillStyle(3005);
-  hPtDistINT7PhysSelAll->SetMinimum(0.5);
-  hPtDistINT7PhysSelAll->Draw();
-  hPtDistVHMSH2PhysSelAll->Draw("same");
-  latexINT7->DrawLatex(0.5,0.65,"INT7");
-  latexVHMSH2->DrawLatex(0.5,0.7,"VHMSH2");
-
-  canPtTurn->cd(2);
-  gPad->SetLogy();
-  hPtDistINT7PhysSelThrs90->SetLineColor(kBlue);
-  hPtDistINT7PhysSelThrs90->SetFillColor(kBlue);
-  hPtDistINT7PhysSelThrs90->SetFillStyle(3004);
-  hPtDistVHMSH2PhysSelThrs90->SetLineColor(kRed);
-  hPtDistVHMSH2PhysSelThrs90->SetFillColor(kRed);
-  hPtDistVHMSH2PhysSelThrs90->SetFillStyle(3005);
-  hPtDistINT7PhysSelThrs90->SetMinimum(0.5);
-  hPtDistINT7PhysSelThrs90->Draw();
-  hPtDistVHMSH2PhysSelThrs90->Draw("same");
-  latexINT7->DrawLatex(0.5,0.65,"INT7");
-  latexVHMSH2->DrawLatex(0.5,0.7,"VHMSH2");
-
-  canPtTurn->cd(3);
-  gPad->SetLogy();
-  hPtDistINT7PhysSelThrs95->SetLineColor(kBlue);
-  hPtDistINT7PhysSelThrs95->SetFillColor(kBlue);
-  hPtDistINT7PhysSelThrs95->SetFillStyle(3004);
-  hPtDistVHMSH2PhysSelThrs95->SetLineColor(kRed);
-  hPtDistVHMSH2PhysSelThrs95->SetFillColor(kRed);
-  hPtDistVHMSH2PhysSelThrs95->SetFillStyle(3005);
-  hPtDistINT7PhysSelThrs95->SetMinimum(0.5);
-  hPtDistINT7PhysSelThrs95->Draw();
-  hPtDistVHMSH2PhysSelThrs95->Draw("same");
-  latexINT7->DrawLatex(0.5,0.65,"INT7");
-  latexVHMSH2->DrawLatex(0.5,0.7,"VHMSH2");
-
-  canPtTurn->cd(4);
-  hPtTurnOnAll->SetMarkerStyle(kOpenCircle);
-  hPtTurnOnAll->SetMarkerSize(0.8);
-  hPtTurnOnAll->SetMarkerColor(kRed);
-  hPtTurnOnAll->SetMinimum(0.);
-  hPtTurnOnAll->SetMaximum(1.05);
-  hPtTurnOnAll->SetStats(0);
-  hPtTurnOnAll->Draw("p0");
-
-  canPtTurn->cd(5);
-  hPtTurnOnThrs90->SetMarkerStyle(kOpenCircle);
-  hPtTurnOnThrs90->SetMarkerSize(0.8);
-  hPtTurnOnThrs90->SetMarkerColor(kRed);
-  hPtTurnOnThrs90->SetMinimum(0.);
-  hPtTurnOnThrs90->SetMaximum(1.05);
-  hPtTurnOnThrs90->SetStats(0);
-  hPtTurnOnThrs90->Draw("p0");
-
-  canPtTurn->cd(6);
-  hPtTurnOnThrs95->SetMarkerStyle(kOpenCircle);
-  hPtTurnOnThrs95->SetMarkerSize(0.8);
-  hPtTurnOnThrs95->SetMarkerColor(kRed);
-  hPtTurnOnThrs95->SetMinimum(0.);
-  hPtTurnOnThrs95->SetMaximum(1.05);
-  hPtTurnOnThrs95->SetStats(0);
-  hPtTurnOnThrs95->Draw("p0");
-  canPtTurn->SaveAs(Form("%s/canPtTurn.pdf",sOutputPath.Data()),"pdf");
-
-
-  // mult & FOR
-  TCanvas* canFOR = new TCanvas("canFOR","canFOR",1000,1000);
-  canFOR->Divide(2,2);
-  canFOR->cd(1);
-  gPad->SetLogz();
-  h2FOROnOffINT7->SetMinimum(1);
-  h2FOROnOffINT7->SetStats(0);
-  h2FOROnOffINT7->Draw("colz");
-  canFOR->cd(2);
-  gPad->SetLogz();
-  h2FOROnOffVHMSH2->SetMinimum(1);
-  h2FOROnOffVHMSH2->SetStats(0);
-  h2FOROnOffVHMSH2->Draw("colz");
-  canFOR->cd(3);
-  gPad->SetLogz();
-  h2FOROnOffINT7PhysSel->SetMinimum(1);
-  h2FOROnOffINT7PhysSel->SetStats(0);
-  h2FOROnOffINT7PhysSel->Draw("colz");
-  canFOR->cd(4);
-  gPad->SetLogz();
-  h2FOROnOffVHMSH2PhysSel->SetMinimum(1);
-  h2FOROnOffVHMSH2PhysSel->SetStats(0);
-  h2FOROnOffVHMSH2PhysSel->Draw("colz");
-  canFOR->SaveAs(Form("%s/canFOR.pdf",sOutputPath.Data()),"pdf");
-
-  TCanvas* canFORtracklets = new TCanvas("canFORtracklets","canFORtracklets",1000,1000);
-  canFORtracklets->Divide(2,2);
-  canFORtracklets->cd(1);
-  gPad->SetLogz();
-  h2FOROffTrackletsINT7PhysSel->SetMinimum(1);
-  h2FOROffTrackletsINT7PhysSel->SetStats(0);
-  h2FOROffTrackletsINT7PhysSel->Draw("colz");
-  canFORtracklets->cd(2);
-  gPad->SetLogz();
-  h2FOROffTrackletsVHMSH2PhysSel->SetMinimum(1);
-  h2FOROffTrackletsVHMSH2PhysSel->SetStats(0);
-  h2FOROffTrackletsVHMSH2PhysSel->Draw("colz");
-  canFORtracklets->cd(3);
-  gPad->SetLogz();
-  h2TrackletsTracksINT7PhysSel->SetMinimum(1);
-  h2TrackletsTracksINT7PhysSel->SetStats(0);
-  h2TrackletsTracksINT7PhysSel->Draw("colz");
-  canFORtracklets->cd(4);
-  gPad->SetLogz();
-  h2TrackletsTracksVHMSH2PhysSel->SetMinimum(1);
-  h2TrackletsTracksVHMSH2PhysSel->SetStats(0);
-  h2TrackletsTracksVHMSH2PhysSel->Draw("colz");
-  canFORtracklets->SaveAs(Form("%s/canFORtracklets.pdf",sOutputPath.Data()),"pdf");
-
+  
   return;
 }
 //_____________________________________________________________________________
