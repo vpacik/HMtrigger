842e7817a10ddd500cb37a74dcb0b8a08f9a5df9
vpacik <vojtech.pacik@cern.ch>
2018-05-31 13:33:57 +0200

    Header disclaimer added

--- newTask/AliAnalysisTaskFilterTrigHMSPD.cxx
+++ newTask/AliAnalysisTaskFilterTrigHMSPD.cxx
@@ -1,3 +1,21 @@
+/*************************************************************************
+* Copyright(c) 1998-2008, ALICE Experiment at CERN, All rights reserved. *
+*                                                                        *
+* Author: The ALICE Off-line Project.                                    *
+* Contributors are mentioned in the code where appropriate.              *
+*                                                                        *
+* Permission to use, copy, modify and distribute this software and its   *
+* documentation strictly for non-commercial purposes is hereby granted   *
+* without fee, provided that the above copyright notice appears in all   *
+* copies and that both the copyright notice and this permission notice   *
+* appear in the supporting documentation. The authors make no claims     *
+* about the suitability of this software for any purpose. It is          *
+* provided "as is" without express or implied warranty.                  *
+**************************************************************************/
+
+// Task for SPD HM trigger study
+// Vojtech Pacik (vojtech.pacik@cern.ch)
+
 #include "AliAnalysisTaskFilterTrigHMSPD.h"
 // #include "AliEventCuts.h"
 #include "AliESDInputHandler.h"
@@ -9,7 +27,6 @@
 #include "AliESDTZERO.h"
 #include "AliVVZERO.h"
 
-// root
 #include "TChain.h"
 #include "TList.h"
 #include "TBits.h"
