211cb0e047d5211b2cbdec190cc150788c0622c1
vpacik <vojtech.pacik@cern.ch>
2018-06-07 12:14:52 +0200

    prepToRun modified

--- newTask/prepToRun.sh
+++ newTask/prepToRun.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-dir="../running/16k-tof"
+tag="16l-pass2"
 
+dir="/Users/vpacik/Codes/ALICE/HMtrigger/running/${tag}/"
+input="/Users/vpacik/Codes/ALICE/HMtrigger/newTask"
 
 mkdir -pv ${dir}
 
-cp ./AddTaskFilterTrigHMSPD.C ${dir}
-cp ./AliAnalysisTaskFilterTrigHMSPD.cxx ${dir}
-cp ./AliAnalysisTaskFilterTrigHMSPD.h ${dir}
-cp ./runEsd.C ${dir}
-cp ./cleanAfterMerge.sh ${dir}
-cp ./mergeOnGrid.sh ${dir}
-cp ./mergeLocal.sh ${dir}
+cp ${input}/AddTaskFilterTrigHMSPD.C ${dir}
+cp ${input}/AliAnalysisTaskFilterTrigHMSPD.cxx ${dir}
+cp ${input}/AliAnalysisTaskFilterTrigHMSPD.h ${dir}
+cp ${input}/runEsd.C ${dir}
+cp ${input}/cleanAfterMerge.sh ${dir}
+cp ${input}/mergeOnGrid.sh ${dir}
+cp ${input}/mergeLocal.sh ${dir}
